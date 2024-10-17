75bdb525d878821f79b91ff4ab82736ebec4601a
gun15992 <gun15992@gmail.com>
2024-10-17 15:30:23 +0700

    Edit test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,32 +1,19 @@
 // Import Libraries
 import { defineConfig } from 'vite'
+import strip from 'vite-plugin-strip'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   build: {
-    sourcemap: true, 
+    minify: true,
+    sourcemap: false,
+    target: 'modules',
   },
-
-  server: {
-    // Domain Name Configuration
-    // host: 'http://localhost:8000/',
-    // port: 5173,
-
-    // Proxy Configuration
-    // proxy: {
-    //     '/api': {
-    //         target: 'http://localhost:8000/',
-    //         changeOrigin: true,
-    //         secure: true
-    //     },
-    // },
-
-    // HTTPS Configuration
-    // https: {
-    //   key: './certs/cert.key',
-    //   cert: './certs/cert.crt'
-    // }
-  },
-
-  plugins: [react()]
+  base: '/web-app/',
+  plugins: [
+    react(),
+    strip({
+      functions: ['use client'],
+    }),
+  ]
 })
